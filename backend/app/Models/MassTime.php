e55d8901d86e874b844323564517a597dec56844
dunzaaay <dsjayasinghe02@gmail.com>
2026-03-11 21:47:17 +0000

    Improve MassTime model with table definition and casting

--- backend/app/Models/MassTime.php
+++ backend/app/Models/MassTime.php
@@ -6,7 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class MassTime extends Model
 {
-    // Allow mass assignment for these fields 
+    /**
+     * --------------------------------------------------------------------------
+     * Table Name
+     * --------------------------------------------------------------------------
+     * Explicitly define the table name used by this model.
+     */
+
+    protected $table = 'mass_times';
+
+    /**
+     * --------------------------------------------------------------------------
+     * Mass Assignable Fields
+     * --------------------------------------------------------------------------
+     * These fields can be filled using create() or update().
+     */
+
     protected $fillable = [
         'day',
         'start_time',
@@ -16,5 +31,17 @@ class MassTime extends Model
         'notes',
         'status',
         'sort_order',
+    ];
+
+    /**
+     * --------------------------------------------------------------------------
+     * Attribute Casting
+     * --------------------------------------------------------------------------
+     * Automatically cast database fields to correct types.
+     */
+
+    protected $casts = [
+        'start_time' => 'datetime:H:i',
+        'end_time'   => 'datetime:H:i',
     ];
 }
